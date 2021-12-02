4cec3c0756712d7f6486f4daa13deec9584bf589
SC-bee <57646327+SC-bee@users.noreply.github.com>
2021-12-03 00:30:58 +0900

    4.3 of django tutorial is finished

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
+from polls.models import Question
+
 from . import views
 
 urlpatterns = [
+    #ex: /polls/
+    #polls/ にアクセスするとviews.indexを呼び出す
     path('', views.index, name='index'),
+
+    #ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #ex: /polls/results/5
+    path('<int:question_id>/results/', views.results, name='results'),
+    #ex: /polls/vote/5
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Question
+
 # Create your views here.
 
+#インデックスページ
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Question を order_by(-pub_date)でソートし,前から5つの配列を取得(降順)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    #リスト内法表記,以下と同じ意味
+    #queston_array = []
+    #for q in latest_question_list:
+    #   question_array.append(q.question_text)
+    #output = ', '.join(question_array)
+
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+#詳細ページ
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+#結果ページ
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response .format(question_id))
+
+#投票ページ
+def vote(request, question_id):
+    return HttpResponse("You're vooting on question {}.".format(question_id))
