e7dc9065e22d0a248b8f90826b86e983fece6cd2
SC-bee <57646327+SC-bee@users.noreply.github.com>
2021-12-04 01:41:01 +0900

    4.5 of django tutorial is finished

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+#from django.template import loader
 from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -15,13 +16,27 @@ def index(request):
     #   question_array.append(q.question_text)
     #output = ', '.join(question_array)
 
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    #テンプレートの読み込み
+    #template = loader.get_template('polls/index.html')
+
+    #テンプレートで使う変数を設定
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 #詳細ページ
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    # try:
+    #     questtion = Question.objects.get(pk=question_id)
+    #     #主キーはそのカラム名に関わらず pk= で検索できる
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    # return render(request, 'polls/detail.html', {'question': question})
 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,'polls.detail.html', {'question': question})
+    
 #結果ページ
 def results(request, question_id):
     response = "You're looking at the results of question {}."
